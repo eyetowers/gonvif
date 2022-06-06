763610c4df7aac47910d46d233b675a52bc8a653
Ondrej Sery <sery.ondra@gmail.com>
2022-06-06 09:46:20 +0200

    Generated Onvif PTZ client.

---
+++ cmd/gonvif/completion/cmd.go
@@ -0,0 +1,32 @@
+package completion
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var Command = &cobra.Command{
+	Use:                   "completion [bash|zsh|fish|powershell]",
+	Short:                 "Generate completion script",
+	Long:                  "To load completions",
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		switch args[0] {
+		case "bash":
+			err = cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			err = cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			err = cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			err = cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+		}
+		if err != nil {
+			os.Exit(1)
+		}
+	},
+}
