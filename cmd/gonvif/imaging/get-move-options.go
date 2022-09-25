61f0ecda8325fc961429cd8d6110fece2de60e5a
Ondrej Sery <sery.ondra@gmail.com>
2022-09-25 14:28:24 +0200

    Add imaging get-move-options command.

---
+++ cmd/gonvif/imaging/get-move-options.go
@@ -0,0 +1,37 @@
+package media
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/eyetowers/gonvif/cmd/gonvif/root"
+	"github.com/eyetowers/gonvif/pkg/generated/onvif/www_onvif_org/ver20/imaging/wsdl"
+	"github.com/eyetowers/gonvif/pkg/util"
+)
+
+var getMoveOptions = &cobra.Command{
+	Use:   "get-move-options",
+	Short: "Show Onvif device focus move options",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := ServiceClient(root.URL, root.Username, root.Password, root.Verbose)
+		if err != nil {
+			return nil
+		}
+		return runGetMoveOptions(client, videoSourceToken)
+	},
+}
+
+func init() {
+	getMoveOptions.Flags().StringVarP(&videoSourceToken, "video_source_token", "t", "", "Video source token")
+	getMoveOptions.MarkFlagRequired("video_source_token")
+}
+
+func runGetMoveOptions(client wsdl.ImagingPort, videoSourceToken string) error {
+	resp, err := client.GetMoveOptions(&wsdl.GetMoveOptions{
+		VideoSourceToken: util.NewReferenceTokenPtr(videoSourceToken),
+	})
+	if err != nil {
+		return err
+	}
+	return root.OutputJSON(resp)
+}
