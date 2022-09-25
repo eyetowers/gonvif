80790223bd93949e2d16b1bd1be5b29699f03f6d
Ondrej Sery <sery.ondra@gmail.com>
2022-09-25 19:26:58 +0200

    Add imaging set-imaging-settings-auto-focus command.

--- cmd/gonvif/imaging/cmd.go
+++ cmd/gonvif/imaging/cmd.go
@@ -22,6 +22,7 @@ func init() {
 		getServiceCapabilities,
 		getStatus,
 		moveContinuous,
+		setImagingSettingsAutoFocus,
 	)
 }
 
---
+++ cmd/gonvif/imaging/set-imaging-settings-auto-focus.go
@@ -0,0 +1,56 @@
+package media
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/eyetowers/gonvif/cmd/gonvif/root"
+	"github.com/eyetowers/gonvif/pkg/generated/onvif/www_onvif_org/ver10/schema"
+	"github.com/eyetowers/gonvif/pkg/generated/onvif/www_onvif_org/ver20/imaging/wsdl"
+	"github.com/eyetowers/gonvif/pkg/util"
+)
+
+var (
+	autoFocus string
+)
+
+var setImagingSettingsAutoFocus = &cobra.Command{
+	Use:   "set-imaging-settings-auto-focus",
+	Short: "Set Onvif device imaging settings autofocus",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := ServiceClient(root.URL, root.Username, root.Password, root.Verbose)
+		if err != nil {
+			return nil
+		}
+		return runSetImagingSettingsAutoFocus(client, videoSourceToken, autoFocus)
+	},
+}
+
+func init() {
+	setImagingSettingsAutoFocus.Flags().StringVarP(&videoSourceToken, "video_source_token", "t", "", "Video source token")
+	setImagingSettingsAutoFocus.MarkFlagRequired("video_source_token")
+	setImagingSettingsAutoFocus.Flags().StringVarP(&autoFocus, "auto_focus", "f", "AUTO", "Auto focus mode")
+}
+
+func runSetImagingSettingsAutoFocus(client wsdl.ImagingPort, videoSourceToken string, autoFocus string) error {
+	// First, read the current settings.
+	original, err := client.GetImagingSettings(&wsdl.GetImagingSettings{
+		VideoSourceToken: util.NewReferenceTokenPtr(videoSourceToken),
+	})
+	if err != nil {
+		return err
+	}
+
+	req := &wsdl.SetImagingSettings{
+		VideoSourceToken: util.NewReferenceTokenPtr(videoSourceToken),
+		ImagingSettings:  original.ImagingSettings,
+	}
+	af := schema.AutoFocusMode(autoFocus)
+	req.ImagingSettings.Focus.AutoFocusMode = &af
+
+	resp, err := client.SetImagingSettings(req)
+	if err != nil {
+		return err
+	}
+	return root.OutputJSON(resp)
+}
