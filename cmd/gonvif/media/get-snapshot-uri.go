70f9a2a06849a72613d7b39a16583b7a86b0ae53
Ondrej Sery <sery.ondra@gmail.com>
2022-09-29 10:56:13 +0200

    Add media get-snapshot-uri command.

---
+++ cmd/gonvif/media/get-snapshot-uri.go
@@ -0,0 +1,37 @@
+package media
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/eyetowers/gonvif/cmd/gonvif/root"
+	"github.com/eyetowers/gonvif/pkg/generated/onvif/www_onvif_org/ver20/media/wsdl"
+	"github.com/eyetowers/gonvif/pkg/util"
+)
+
+var getSnapshotURI = &cobra.Command{
+	Use:   "get-snapshot-uri",
+	Short: "Get Onvif device media snapshot URI",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := ServiceClient(root.URL, root.Username, root.Password, root.Verbose)
+		if err != nil {
+			return nil
+		}
+		return runGetSnapshotURI(client, profileToken)
+	},
+}
+
+func init() {
+	getSnapshotURI.Flags().StringVarP(&profileToken, "profile_token", "t", "", "The ProfileToken element indicates the media profile to use and will define the source and dimensions of the snapshot.")
+	root.MustMarkFlagRequired(getSnapshotURI, "profile_token")
+}
+
+func runGetSnapshotURI(client wsdl.Media2, profileToken string) error {
+	resp, err := client.GetSnapshotUri(&wsdl.GetSnapshotUri{
+		ProfileToken: util.NewReferenceTokenPtr(profileToken),
+	})
+	if err != nil {
+		return err
+	}
+	return root.OutputJSON(resp)
+}
